740615d943df4bcd93a7f9fff10550fc69d89069
gsp <cgsp740178088@163.com>
2018-07-07 21:59:13 +0800

    晚上第一提交

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -1,1 +1,72 @@
-Page({});
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {},
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    const res = {
+      date: 'Sep 18 2016',
+      title: '正是虾肥螃蟹壮',
+      post_image: '/images/avatar.png',
+      content: '正是虾肥蟹胖时正是虾肥蟹胖时正是虾肥蟹胖时正是虾肥蟹胖时正是虾肥蟹胖时正是虾肥蟹胖时正是虾肥蟹胖时正是虾肥蟹胖时正是虾肥蟹胖时正是虾肥蟹胖时正是虾肥蟹胖时',
+      view_num: 112,
+      collect_num: 96,
+      author_image: '/images/avatar.png'
+    }
+    this.setData(res);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
