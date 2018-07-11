b5d6e69e173a7bd938b35c5bb78d34051ec8c2df
guansaipeng <cgsp740178088@126.com>
2018-07-11 17:02:37 +0800

    提交111

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,80 +1,65 @@
 Page({
 
-  open() {
-    wx.switchTab({
-      url: '../posts/posts',
-      success() {
-        console.log('成功')
-      },
-      fail() {
-        console.log('失败')
-      },
-      complete() {
-        console.log('完成')
-      },
-    })
-  },
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
-
+  onLoad: function (options) {
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
-
+  onReady: function () {
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
-
+  onShow: function () {
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
-    console.log('欢迎页被隐藏了');
+  onHide: function () {
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
-    console.log('欢迎页被卸载了');
+  onUnload: function () {
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
-
+  onPullDownRefresh: function () {
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
-
+  onReachBottom: function () {
+    
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
-
+  onShareAppMessage: function () {
+    
   }
 })
